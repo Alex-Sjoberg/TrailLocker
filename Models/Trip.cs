6629cea82904c91d67be2053666ef5d250fb5e88
Andrew Smith <andrew_smith3@taylor.edu>
2013-09-10 00:05:29 -0400

    Added Trip.cs model
    Constructor will be better implemented later

---
+++ Models/Trip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestLocker.Models
+{   
+    public class Trip
+    {
+       
+        public String TripName { get; set; }
+        public String DestinationName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+
+        public Trip(string destinationName)
+        {
+            this.DestinationName = destinationName;
+        }
+
+        public String toString()
+        {
+            return this.DestinationName;
+        }
+    }
+}

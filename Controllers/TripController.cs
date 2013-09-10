5835fe899aa953b81843597293cda33ccb3ec394
bsell93 <bryantsell@gmail.com>
2013-09-10 00:04:28 -0400

    Added TripController.cs

---
+++ Controllers/TripController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrailLocker.Controllers
+{
+    public class TripController : Controller
+    {
+        //
+        // GET: /Trip/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // PUT: /Trip/AddTrip
+        [HttpPut]
+        public ActionResult AddTrip(string TripName, string DestinationName, DateTime StartDate, DateTime EndDate, double Latitude, double Longitude)
+        {
+            return View();
+        }
+
+    }
+}

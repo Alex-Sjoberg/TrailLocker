37d29d3e3fa52c887af525be35cc5585ee5b2641
Seth Foote <seth_foote@taylor.edu>
2013-09-24 16:56:03 -0400

    Added create, edit, and delete logic for locker

--- Controllers/EquipmentController.cs
+++ Controllers/EquipmentController.cs
@@ -3,27 +3,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 using TrailLocker.Models;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace TrailLocker.Controllers
 {
     public class EquipmentController : Controller
     {
-
+        List<EquipmentModel> equipmentRepository;
+        List<EquipmentModel> defaultRepository;
+        BinaryFormatter bformatter = new BinaryFormatter();
+        public String equipmentFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\equipmentRepository.txt";
+        public String defaultFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\defualtRepository.txt";
 
         //
         // GET: /Equipment/
-
         public ActionResult Index()
+        {
+            equipmentRepository = readList(equipmentFile, equipmentRepository);
+            return View(equipmentRepository.AsQueryable());
+        }
+
+        //
+        // GET: /Equipment/add
+        public ActionResult Add()
         {
             return View();
         }
 
+        //
+        // POST: /Home/add
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(EquipmentModel equipmentModel)
+        {
+            equipmentRepository = readList(equipmentFile, equipmentRepository);
+            equipmentModel.EquipmentID = equipmentRepository.Count;
+            equipmentRepository.Add(equipmentModel);
+            writeList(equipmentFile, equipmentRepository);
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Home/Default/5
+        public ActionResult Default(int EquipmentID = 0)
+        {
+            equipmentRepository = readList(equipmentFile, equipmentRepository);
+            defaultRepository = readList(defaultFile, defaultRepository);
+            EquipmentModel equipmentModel = equipmentRepository.Find(items => items.EquipmentID == EquipmentID);
+            if (equipmentModel == null)
+            {
+                return HttpNotFound();
+            }
+            defaultRepository.Add(equipmentModel);
+            writeList(defaultFile, defaultRepository);
+            return View(defaultRepository.AsQueryable());
+        }
+
+        //
+        // GET: /Home/Edit/5
+        public ActionResult Edit(int EquipmentID = 0)
+        {
+            equipmentRepository = readList(equipmentFile, equipmentRepository);
+            EquipmentModel equipmentModel = equipmentRepository.Find(items => items.EquipmentID == EquipmentID);
+            if (equipmentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipmentModel);
+        }
+
+        //
+        // POST: /Home/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EquipmentModel equipmentModel)
+        {
+            equipmentRepository = readList(equipmentFile, equipmentRepository);
+            equipmentRepository.RemoveAll(items => items.EquipmentID == equipmentModel.EquipmentID);
+            equipmentRepository.Add(equipmentModel);
+            writeList(equipmentFile, equipmentRepository);
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Home/Delete/5
+        public ActionResult Delete(int EquipmentID = 0)
+        {
+            equipmentRepository = readList(equipmentFile, equipmentRepository);
+            equipmentRepository.RemoveAll(items => items.EquipmentID == EquipmentID);
+            writeList(equipmentFile, equipmentRepository);
+            return RedirectToAction("Index");
+        }
+
+
+
         // GET: /Equipment/search
         public ActionResult Search(EquipmentSearchModel searchParams)
         {
-            IQueryable<EquipmentModel> CompleteLocker = CreateEquipment();
+            //populateEquipmentRepository();
+            IQueryable<EquipmentModel> CompleteLocker = readList(equipmentFile, equipmentRepository).AsQueryable();
+
+            //IQueryable<EquipmentModel> CompleteLocker = CreateEquipment();
 
             if (!String.IsNullOrEmpty(searchParams.Name))
             {
@@ -52,13 +135,47 @@ namespace TrailLocker.Controllers
         {
             List<EquipmentModel> equipment = new List<EquipmentModel>();
 
-            equipment.Add(new EquipmentModel(){Name = "Sleeping Bag", Weight = 2.0, Category = EquipmentCategory.Other, EquipmentID = 1});
-            equipment.Add(new EquipmentModel(){Name = "Pizza", Weight = 120, Category = EquipmentCategory.Perishable, EquipmentID = 2});
-            equipment.Add(new EquipmentModel(){Name = "Water Bottle", Weight = 10, Category = EquipmentCategory.Expendable, EquipmentID = 3});
-            equipment.Add(new EquipmentModel(){Name = "Backpack", Weight = 50, Category = EquipmentCategory.Backpack, EquipmentID = 4});
+            equipment.Add(new EquipmentModel() { Name = "Sleeping Bag", Weight = 2.0, Category = EquipmentCategory.Other, EquipmentID = 1 });
+            equipment.Add(new EquipmentModel() { Name = "Pizza", Weight = 120, Category = EquipmentCategory.Perishable, EquipmentID = 2 });
+            equipment.Add(new EquipmentModel() { Name = "Water Bottle", Weight = 10, Category = EquipmentCategory.Expendable, EquipmentID = 3 });
+            equipment.Add(new EquipmentModel() { Name = "Backpack", Weight = 50, Category = EquipmentCategory.Backpack, EquipmentID = 4 });
             equipment.Add(new EquipmentModel() { Name = "Chips", Weight = 5, Category = EquipmentCategory.Perishable, EquipmentID = 5 });
 
             return equipment.AsQueryable();
+        }
+
+        private void populateEquipmentRepository()
+        {
+            equipmentRepository = new List<EquipmentModel>();
+            equipmentRepository.Add(new EquipmentModel() { Name = "Sleeping Bag", Weight = 2.0, Category = EquipmentCategory.Other, EquipmentID = 1 });
+            equipmentRepository.Add(new EquipmentModel() { Name = "Pizza", Weight = 120, Category = EquipmentCategory.Perishable, EquipmentID = 2 });
+            equipmentRepository.Add(new EquipmentModel() { Name = "Water Bottle", Weight = 10, Category = EquipmentCategory.Expendable, EquipmentID = 3 });
+            equipmentRepository.Add(new EquipmentModel() { Name = "Backpack", Weight = 50, Category = EquipmentCategory.Backpack, EquipmentID = 4 });
+            equipmentRepository.Add(new EquipmentModel() { Name = "Chips", Weight = 5, Category = EquipmentCategory.Perishable, EquipmentID = 5 });
+            writeList(equipmentFile, equipmentRepository);
+        }
+
+        private List<EquipmentModel> readList(String filename, List<EquipmentModel> repository)
+        {
+            if (!System.IO.File.Exists(filename))
+            {
+                repository = new List<EquipmentModel>();
+            }
+            //read equipmentRepository in from file test.txt
+            else
+            {
+                Stream readStream = System.IO.File.Open(filename, FileMode.Open);
+                repository = (List<EquipmentModel>)bformatter.Deserialize(readStream);
+                readStream.Close();
+            }
+            return repository;
+        }
+
+        private void writeList(String filename, List<EquipmentModel> repository)
+        {
+            Stream writeStream = System.IO.File.Open(filename, FileMode.Create);
+            bformatter.Serialize(writeStream, repository);
+            writeStream.Close();
         }
     }
 }
--- Models/EquipmentModel.cs
+++ Models/EquipmentModel.cs
@@ -2,25 +2,108 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace TrailLocker.Models
 {
     public enum EquipmentCategory
     {
-        Backpack    = 0x00000001,
-        Perishable  = 0x00000002,
-        Expendable  = 0x00000003,
-        Other       = 0x00000004
+        Backpack = 0x00000001,
+        Perishable = 0x00000002,
+        Expendable = 0x00000003,
+        Tent = 0x00000004,
+        SleepingBag = 0x00000005,
+        Other = 0x00000006
     }
 
     //public enum EquipmentRating
 
-    public class EquipmentModel
+    [Serializable()]
+    public class EquipmentModel : ISerializable
     {
         public Int32 EquipmentID { get; set; }
         public String Name { get; set; }
         public EquipmentCategory Category { get; set; }
         public Double Weight { get; set; }
         public String Location { get; set; }
+        public Int32 Quantity { get; set; }
+
+        //no argument constructor for ISerializable functionality
+        public EquipmentModel()
+        {
+            EquipmentID = 0;
+            Name = null;
+            Category = EquipmentCategory.Other;
+            Weight = 0;
+            Location = null;
+            Quantity = 0;
+        }
+
+        //serialization function to read from file
+        public EquipmentModel(SerializationInfo info, StreamingContext ctxt)
+        {
+            this.EquipmentID = (Int32)info.GetValue("EquipmentID", typeof(Int32));
+            this.Name = (String)info.GetValue("Name", typeof(String));
+            this.Category = (EquipmentCategory)info.GetValue("Category", typeof(EquipmentCategory));
+            this.Weight = (Double)info.GetValue("Weight", typeof(Double));
+            this.Location = (String)info.GetValue("Location", typeof(String));
+            this.Quantity = (Int32)info.GetValue("Quantity", typeof(Int32));
+        }
+
+        //serialization function to write to file
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("EquipmentID", EquipmentID);
+            info.AddValue("Name", Name);
+            info.AddValue("Category", Category);
+            info.AddValue("Weight", Weight);
+            info.AddValue("Location", Location);
+            info.AddValue("Quantity", Quantity);
+        }
+    }
+
+    // DestinationImages
+    // Destiation Coordinates
+    // 
+
+    public class BackpackEquipmentModel : EquipmentModel
+    {
+        public Double Capacity { get; set; }
+
+        //public String Size
+        //{
+        //    get
+        //    {
+        //        //if (Volume < 20.0)
+        //        //{
+        //        //    return "Small";
+        //        //}
+        //        //else if (Volume < 50.0)
+        //        //{
+        //        //    return "Medium";
+        //        //}
+        //    }
+        //}
+    }
+
+    public class PerishableEquipmentModel : EquipmentModel
+    {
+        public DateTime ExpireDate { get; set; }
+    }
+
+    public class ExpendableEquipmentModel : EquipmentModel
+    {
+        public Int32 UsesRemaining { get; set; }
+    }
+
+    public class TentEquipmentModel : EquipmentModel
+    {
+        public Int32 SleepingCapacity { get; set; }
+    }
+
+    public class SleepingBagEquipmentModel : EquipmentModel
+    {
+        public Double MinimumTemperature { get; set; }
     }
 }

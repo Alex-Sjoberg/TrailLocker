25f44cd13fdc7417eb12bc36646e4689b70ed163
bsell93 <bryantsell@gmail.com>
2013-10-03 16:59:52 -0400

    Changed Index page to have links to Trips and Equipment

--- TrailLocker/Controllers/TripController.cs
+++ TrailLocker/Controllers/TripController.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using COS340.TrailLocker.Data;
 
 using TrailLocker.Models;
 
@@ -13,12 +16,21 @@ namespace TrailLocker.Controllers
 {
     public class TripController : Controller
     {
+        Repository<TripModel> tripRepository;
+        InMemoryUnitOfWork unitOfWork;
+        
+        public TripController()
+        {
+            unitOfWork = new InMemoryUnitOfWork();
+            tripRepository = new Repository<TripModel>(unitOfWork);
+        }
         
         // GET: /Trip/Trips
         // Routed to by GET: /Trip/
         public ActionResult Trips()
         {
-            return View(CreateTrips());
+            CreateTrips();
+            return View(tripRepository.FindAll());
         }
 
         //Gets view for adding trip
@@ -33,13 +45,15 @@ namespace TrailLocker.Controllers
         public ActionResult AddTrip(TripModel trip)
         {
             //Add trip to database
-            return RedirectToAction("Trips");
+            tripRepository.Add(trip);
+            return RedirectToAction("Index");
         }
 
         //GET: /Trip/EditTrip
         [HttpGet]
         public ActionResult EditTrip(TripModel trip)
         {
+            tripRepository.Remove(trip);
             return View(trip);
         }
 
@@ -48,6 +62,7 @@ namespace TrailLocker.Controllers
         public ActionResult EditTripInDatabase(TripModel trip)
         {
             //edit trip in database
+            tripRepository.Add(trip);
             return RedirectToAction("Trips");
         }
 
@@ -56,17 +71,38 @@ namespace TrailLocker.Controllers
         public ActionResult DeleteTrip(TripModel trip)
         {
             //Delete trip from database
+            tripRepository.Remove(trip);
             return RedirectToAction("Trips");
         }
 
-        public List<TripModel> CreateTrips()
+        public Repository<TripModel> CreateTrips()
         {
-            List<TripModel> tripsList = new List<TripModel>();
-            tripsList.Add(new TripModel() { TripName = "Fall 2012", DestinationName = "Colorado", StartYear = 2012, StartMonth = 9, StartDay = 15, EndYear = 2012, EndMonth = 9, EndDay = 19 });
-            tripsList.Add(new TripModel() { TripName = "Fall 2013", DestinationName = "Florida", StartYear = 2013, StartMonth = 10, StartDay = 18, EndYear = 2013, EndMonth = 10, EndDay = 20 });
-            tripsList.Add(new TripModel() { TripName = "Summer 2013", DestinationName = "Egypt", StartYear = 2013, StartMonth = 6, StartDay = 15, EndYear = 2013, EndMonth = 6, EndDay = 19 });
+            tripRepository.Add(new TripModel() { TripName = "Fall 2012", 
+                                                 DestinationName = "Colorado", 
+                                                 StartYear = 2012, 
+                                                 StartMonth = 9, 
+                                                 StartDay = 15, 
+                                                 EndYear = 2012, 
+                                                 EndMonth = 9, 
+                                                 EndDay = 19 });
+            tripRepository.Add(new TripModel() { TripName = "Fall 2013", 
+                                                 DestinationName = "Florida", 
+                                                 StartYear = 2013, 
+                                                 StartMonth = 10, 
+                                                 StartDay = 18, 
+                                                 EndYear = 2013, 
+                                                 EndMonth = 10, 
+                                                 EndDay = 20 });
+            tripRepository.Add(new TripModel() { TripName = "Summer 2013", 
+                                                 DestinationName = "Egypt", 
+                                                 StartYear = 2013, 
+                                                 StartMonth = 6, 
+                                                 StartDay = 15, 
+                                                 EndYear = 2013, 
+                                                 EndMonth = 6, 
+                                                 EndDay = 19 });
 
-            return tripsList;
+            return tripRepository;
         }
 
     }
--- TrailLocker/Models/TripModel.cs
+++ TrailLocker/Models/TripModel.cs
@@ -5,9 +5,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace TrailLocker.Models
-{   
+{
     public class TripModel
     {
         public String TripName { get; set; }
